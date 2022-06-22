68d52498912c2d0683d306f6ad17011d33e2233b
edjunma <edjunma@gmail.com>
2022-06-22 03:49:55 -0400

    6/22 update

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,4 +21,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.REACT_APP_RINKEBY_RPC_URL,
+      accounts: [process.env.REACT_APP_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.REACT_APP_ETHERSCAN_KEY,
+  },
 };
